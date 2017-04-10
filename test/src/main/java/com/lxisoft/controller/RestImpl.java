18aa10b13843b0b4e45bc79f1561908f644a92f1
ArunJohnson <arun.johnson@lxisoft.com>
2017-04-10 16:35:10 +0530

    fixed initial draft #1

---
+++ test/src/main/java/com/lxisoft/controller/RestImpl.java
@@ -0,0 +1,16 @@
+package com.lxisoft.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestImpl {
+	
+	@RequestMapping("/")
+	public String sayHello()
+	{
+		return "HERE I AM";
+		
+	}
+
+}
